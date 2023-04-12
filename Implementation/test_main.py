9f5d6d85cfdc4c121d8b607f23d7d38c91f22b3d
breferala <brendajfa@outlook.com>
2023-04-12 16:45:32 +0200

    update test_main.py

--- Implementation/test_main.py
+++ Implementation/test_main.py
@@ -1,20 +1,7 @@
-import os
-from datetime import datetime
-import torch
-# from torch.utils.data import DataLoader
-import numpy as np
-import logs
-# import pointdata
-# import model_fm
-# import model_random
-# import model_pop
-# import model_nfc
-import sampling
-import exec
-# import plots
 from main import Main
 
 def test_main():
     dataset = "movie lens"
     main = Main(dataset)
+    main.start()
 
